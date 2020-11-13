3947b521a9200f2dd23fdb1f9d3b9adde3146945
grace <grace.c.youngblood@gmail.com>
2020-11-13 14:53:21 -0500

    use secure address in mailer.

--- app/mailers/session_mailer.rb
+++ app/mailers/session_mailer.rb
@@ -3,7 +3,7 @@ class SessionMailer < ApplicationMailer
 
     def claim
         @session = params[:session]
-        @url = "http://#{ENV["APPLICATION_HOST"]}/session/#{@session.code}"
+        @url = "https://#{ENV["APPLICATION_HOST"]}/session/#{@session.code}"
 
         mail(to: @session.player.email, subject: 'Broken Pixel Games: sign in.')
     end

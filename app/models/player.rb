7633e37684ddd93ad8920c1d38f5588765857a4b
grace <grace.c.youngblood@gmail.com>
2020-11-13 13:02:29 -0500

    recalculate balance in sessions controller.

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,8 +2,12 @@ class Player < ApplicationRecord
     validates :handle, presence: true
     validates :email, presence: true
     has_many :charges
+    has_many :games
 
     def as_json(*args)
-        super(*args).merge(balance: charges.pluck(:price).reduce(:+)) || 0
+        super(*args).merge(balance:
+            charges.pluck(:price).reduce(:+) -
+            games.pluck(:pauses, :speed_drops).map {|p, s| (p||0) * 10 + (s||0) * 25 }.reduce(:+)
+        ) || 0
     end
 end

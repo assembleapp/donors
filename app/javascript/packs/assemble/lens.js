708acce550328a2cc78796c20a5e93dc25c6bab4
grace <grace.c.youngblood@gmail.com>
2020-11-21 18:07:22 -0500

    add border on `<Lens.li>`

--- app/javascript/packs/assemble/lens.js
+++ app/javascript/packs/assemble/lens.js
@@ -1,7 +1,10 @@
 import React from "react"
+import styled from "styled-components"
 
-const Lens = {
-    li: ({ children }) => <li>{children}</li>
-}
+const li = styled.li`
+border: 1px solid #ee4a0a;
+`
+
+const Lens = { li }
 
 export default Lens

e44498d13cdf9431852f35b10fcb656f1cc47839
grace <grace.c.youngblood@gmail.com>
2020-11-21 08:01:49 -0500

    pull charge-related blocks into `charge.js`

--- app/javascript/packs/splash.js
+++ app/javascript/packs/splash.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import styled from "styled-components"
-import { observable, autorun, toJS, runInAction } from "mobx"
 import { observer } from "mobx-react"
 
-import {
-    mdiCreditCardOutline,
-    mdiChevronUp,
-} from "@mdi/js"
+import { mdiCreditCardOutline } from "@mdi/js"
 import { Icon } from "@mdi/react"
 
 import SquareCardArea from "./square"
+import ChargeArea from "./charge"
 import { CellPanel } from "./board"
 import Snake from "./snake"
 import LeaderBoard from "./leaderboard"
@@ -88,41 +85,6 @@ const Splash = () => (
 </Scene>
 )
 
-const ChargeArea = observer(({ session }) => (
-    session.player && session.player.card_summary
-    ?
-        <Area>
-            <Query type="text" placeholder="add money (in USD)" value={price.get()}
-            onChange={(e) => runInAction(() => price.set(e.target.value))} />
-        
-            <ChargeCard onClick={(e) => {e.preventDefault(); chargeCard()}} >
-            Charge card
-            </ChargeCard>
-        </Area>
-    :
-        <Area>
-            <Icon size={1} path={mdiChevronUp} />
-            {session.player ? "Add" : "Sign in and add"} a bank card.
-        </Area>
-))
-
-const price = observable.box("5.00")
-
-const chargeCard = () => {
-    fetch("/card/charge", {
-        method: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'X-CSRF-Token': document.querySelector("meta[name='csrf-token']").content,
-            'Authorization': localStorage.getItem("code"),
-        },
-        body: JSON.stringify({
-            charge: { price: price.get() },
-        }),
-    }).then(() => window.location = window.location)
-}
-
 const Code = styled.code`
 display: inline;
 color: #8c8cd0;
@@ -142,19 +104,6 @@ margin-bottom: 0.5rem;
 
 const Balance = styled.span`
 font-family: sans-serif;
-`
-
-const Query = styled.input`
-font-size: 16px;
-line-height: 24px;
-&::placeholder {
-    color: #a0a0a0;
-}
-color: #e0e0e0;
-background-color: rgba(212, 196, 196, 0.2);
-border: none;
-margin-bottom: 0.2rem;
-outline: none;
 `
 
 const Line = styled.div`
@@ -182,22 +131,6 @@ display: grid;
 grid-template-columns: auto 1fr 2fr auto;
 grid-gap: none;
 color: #ededed;
-`
-
-const Area = styled.div`
-padding: 1rem;
-display: flex;
-flex-direction: column;
-width: 12rem;
-`
-
-const ChargeCard = styled.button`
-width: 100%;
-padding: 0.5rem;
-background-color: rgba(196, 196, 212, 0.6);
-border-radius: 4px;
-outline: none;
-border: none;
 `
 
 export default observer(Splash)

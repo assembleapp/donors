57d97beebb8a538ca1041f145342b2ee55991105
grace <grace.c.youngblood@gmail.com>
2020-11-21 20:08:27 -0500

    change up lens.

--- app/javascript/packs/assemble/lens.js
+++ app/javascript/packs/assemble/lens.js
@@ -1,9 +1,19 @@
 import React from "react"
 import styled from "styled-components"
 
-const li = styled.li`
-border: 1px solid #ee4a0a;
-`
+// const li = styled.li`
+// border: 1px solid #ee4a0a;
+// `
+
+const li = ({children}) => (
+    <input
+    key={children}
+    style={{ display: "block" }}
+    type="text"
+    value={children}
+    onChange={(e) => console.log(e.target.value)}
+    />
+)
 
 const Lens = { li }
 

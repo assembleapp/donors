f1fb810812a6081c42e6431a9b434aad6e4e1ce5
grace <grace.c.youngblood@gmail.com>
2020-11-26 14:11:35 -0500

    only persist changes on [Enter].

--- app/javascript/packs/assemble/lens.js
+++ app/javascript/packs/assemble/lens.js
@@ -1,28 +1,44 @@
 import React from "react"
 import styled from "styled-components"
 
-const change = ({children, source, code}) => (
-    <textarea
-    key={children}
-    style={{ display: "block" }}
-    type="text"
-    value={children}
-    onChange={(e) => fetch("http://0.0.0.0:4321/change", {
-        method: "POST",
-        body: JSON.stringify({
-            upgrade: e.target.value,
-            source,
-            code,
-        }),
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        },
-    })
-    .then(response => response.text())
-    .then(response => console.log(response))}
-    />
-)
+class change extends React.Component {
+    state = { value: null }
+
+    render = () => (
+        <textarea
+        key={this.props.children}
+        style={{ display: "block" }}
+        type="text"
+        value={this.state.value || this.props.children}
+        onChange={(e) => {
+            this.setState({ value: e.target.value })
+        }}
+        onKeyDown={(e) => {
+            if(e.key === "Enter") {
+                fetch("http://0.0.0.0:4321/change", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        upgrade: this.state.value,
+                        source: this.props.source,
+                        code: this.props.code,
+                    }),
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                })
+                .then(response => response.text())
+                .then(response => console.log(response))
+                e.preventDefault();
+                e.stopPropagation();
+                return false
+            } else {
+                console.log(e.key)
+            }
+        }}
+        />
+    )
+}
 
 const Lens = { change }
 

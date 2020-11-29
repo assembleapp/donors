c1208e4663a818db4861f23da715b5d86ff065b5
grace <grace.c.youngblood@gmail.com>
2020-11-29 12:55:06 -0500

    no more re-initializing our `program`.

--- hierarch/parse.js
+++ hierarch/parse.js
@@ -1,29 +1,5 @@
 var fs = require('fs')
-
-const Parser = require("tree-sitter")
-const JavaScript = require("tree-sitter-javascript")
-
-class Program {
-    constructor(source) {
-        this.source = source
-        this.parser = new Parser()
-        this.parser.setLanguage(JavaScript)
-        this.parsed = this.parser.parse(this.source)
-    }
-
-    replace_in_program_by_node(node, upgrade) {
-        this.replace_in_program_by_indices(node.startIndex, node.endIndex, upgrade)
-    }
-
-    replace_in_program_by_indices(begin, end, upgrade) {
-        if(begin < 0) {
-            begin = str.length + begin
-            if(begin < 0) begin = 0
-        }
-
-        this.source = this.source.slice(0, begin) + (upgrade || "") + this.source.slice(end)
-    }
-}
+var Program = require("./program")
 
 const go = (change = null) => {
     var sourceAddress = __dirname + '/../app/javascript/packs/charge.js'
@@ -34,26 +10,26 @@ const go = (change = null) => {
         var source = response
 
         var program = new Program(source)
-        var parsed = program.parsed
 
         // search for, and perhaps insert, necessary import statement
-        var query = new Parser.Query(JavaScript, `
+        var matches = program.query(`
             (import_statement (import_clause (identifier) @identifier) source: (string) @source)
-        `);
-        var matches = query.matches(parsed.rootNode).map(x => x.captures.map(c => parsed.getText(c.node)))
-        if(!matches.some(dependency => dependency[0] === "Lens" && dependency[1].match(/^['"]\.\/assemble\/lens['"]$/))) {
+        `).map(x => x.captures.map(c => program.parsed.getText(c.node)))
+        if(!matches.some(dependency =>
+            dependency[0] === "Lens" &&
+            dependency[1].match(/^['"]\.\/assemble\/lens['"]$/))
+        ) {
             program.replace_in_program_by_indices(0, 0, "import Lens from './assemble/lens'\n")
         }
 
         // single out an element to change
-        program = new Program(program.source)
-        parsed = program.parsed
-        query = new Parser.Query(JavaScript, `(jsx_element
+        matches = program.query(`
+        (jsx_element
             open_tag: (jsx_opening_element name: (identifier) @opening-name)
             close_tag: (jsx_closing_element name: (identifier) @closing-name)
-            )`);
-        matches = query.matches(parsed.rootNode).filter(x =>
-            x.captures.every(c => parsed.getText(c.node) === "ChargeCard")
+        )
+        `).filter(x =>
+            x.captures.every(c => program.parsed.getText(c.node) === "ChargeCard")
         )
         matches.forEach(match => {
             program.replace_in_program_by_node(
@@ -72,9 +48,7 @@ const go = (change = null) => {
             change.source === source_name &&
             change.upgrade
         ) {
-            program = new Program(program.source)
-            parsed = program.parsed
-            query = new Parser.Query(JavaScript, `(jsx_element
+            matches = program.query(`(jsx_element
                 open_tag: (
                     jsx_opening_element
                     name: (nested_identifier) @opening-name
@@ -83,21 +57,20 @@ const go = (change = null) => {
                     )
                 (jsx_text) @children
                 close_tag: (jsx_closing_element name: (nested_identifier) @closing-name)
-            ) @element`);
+            ) @element`)
 
-                //     (#match? @opening-name "Lens\.change")
-                // (#match? @closing-name "Lens\.change")
+            // (#match? @opening-name "Lens\.change")
+            // (#match? @closing-name "Lens\.change")
 
-            matches = query.matches(parsed.rootNode)
-            console.log(matches[0].captures.map(c => parsed.getText(c.node)))
-            // matches = query.matches(parsed.rootNode).filter(x =>
-            //     x.captures.some(c => c.name.match(/-name$/) && parsed.getText(c.node) === "Lens.change")
+            console.log(matches[0].captures.map(c => program.parsed.getText(c.node)))
+            // matches = query.matches(program.parsed.rootNode).filter(x =>
+            //     x.captures.some(c => c.name.match(/-name$/) && program.parsed.getText(c.node) === "Lens.change")
             // )
             //     matches.forEach(match => {
             //         // broken: check against source and code
             //         match.captures.filter(c => c.name === "attr").some(c => {
-            //             var attr_name = parsed.getText(c.node.children[0])
-            //             var attr_value = parsed.getText(c.node.children[2])
+            //             var attr_name = program.parsed.getText(c.node.children[0])
+            //             var attr_value = program.parsed.getText(c.node.children[2])
 
             //             if(attr_name === "code")
             //                 console.log("code", attr_value)
@@ -117,7 +90,7 @@ const go = (change = null) => {
 
             //     // broken: drop opening element attributes
             //     match.captures.filter(c => c.name === "attr").reverse().forEach(c => {
-            //         var attr_name = parsed.getText(c.node.children[0])
+            //         var attr_name = program.parsed.getText(c.node.children[0])
             //         if(attr_name === "code" || attr_name === "source")
             //             program.replace_in_program_by_node(source, c.node, "")
             //     })

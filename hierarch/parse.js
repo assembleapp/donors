7af84b9c2747b03c4cb8247aa04c5f66aeb5f30b
grace <grace.c.youngblood@gmail.com>
2020-11-29 12:24:54 -0500

    modify source in-place using the `Program` class.

--- hierarch/parse.js
+++ hierarch/parse.js
@@ -10,6 +10,19 @@ class Program {
         this.parser.setLanguage(JavaScript)
         this.parsed = this.parser.parse(this.source)
     }
+
+    replace_in_program_by_node(node, upgrade) {
+        this.replace_in_program_by_indices(node.startIndex, node.endIndex, upgrade)
+    }
+
+    replace_in_program_by_indices(begin, end, upgrade) {
+        if(begin < 0) {
+            begin = str.length + begin
+            if(begin < 0) begin = 0
+        }
+
+        this.source = this.source.slice(0, begin) + (upgrade || "") + this.source.slice(end)
+    }
 }
 
 const go = (change = null) => {
@@ -20,30 +33,20 @@ const go = (change = null) => {
         if(error) return console.log(error)
         var source = response
 
-
-        const replace_in_program_by_node = (source, node, upgrade) =>
-            replace_in_program_by_indices(source, node.startIndex, node.endIndex, upgrade)
-
-        const replace_in_program_by_indices = (source, begin, end, upgrade) => {
-            if(begin < 0) {
-                begin = str.length + begin
-                if(begin < 0) begin = 0
-            }
-
-            return source.slice(0, begin) + (upgrade || "") + source.slice(end)
-        }
-
-        // search for, and perhaps insert, necessary import statement
         var program = new Program(source)
         var parsed = program.parsed
-        var query = new Parser.Query(JavaScript, `(import_statement (import_clause (identifier) @identifier) source: (string) @source)`);
+
+        // search for, and perhaps insert, necessary import statement
+        var query = new Parser.Query(JavaScript, `
+            (import_statement (import_clause (identifier) @identifier) source: (string) @source)
+        `);
         var matches = query.matches(parsed.rootNode).map(x => x.captures.map(c => parsed.getText(c.node)))
         if(!matches.some(dependency => dependency[0] === "Lens" && dependency[1].match(/^['"]\.\/assemble\/lens['"]$/))) {
-            source = replace_in_program_by_indices(source, 0, 0, "import Lens from './assemble/lens'\n")
+            program.replace_in_program_by_indices(0, 0, "import Lens from './assemble/lens'\n")
         }
 
         // single out an element to change
-        program = new Program(source)
+        program = new Program(program.source)
         parsed = program.parsed
         query = new Parser.Query(JavaScript, `(jsx_element
             open_tag: (jsx_opening_element name: (identifier) @opening-name)
@@ -53,12 +56,12 @@ const go = (change = null) => {
             x.captures.every(c => parsed.getText(c.node) === "ChargeCard")
         )
         matches.forEach(match => {
-            source = replace_in_program_by_node(
-                source, match.captures.filter(c => c.name === "closing-name")[0].node,
+            program.replace_in_program_by_node(
+                match.captures.filter(c => c.name === "closing-name")[0].node,
                 "Lens.change"
             )
-            source = replace_in_program_by_node(
-                source, match.captures.filter(c => c.name === "opening-name")[0].node,
+            program.replace_in_program_by_node(
+                match.captures.filter(c => c.name === "opening-name")[0].node,
                 `Lens.change source="${source_name}" code="abcd"`
             )
          })
@@ -69,7 +72,7 @@ const go = (change = null) => {
             change.source === source_name &&
             change.upgrade
         ) {
-            program = new Program(source)
+            program = new Program(program.source)
             parsed = program.parsed
             query = new Parser.Query(JavaScript, `(jsx_element
                 open_tag: (
@@ -104,11 +107,10 @@ const go = (change = null) => {
             //                 //     a.value.value === source_name
             //         })
 
-            //     source = replace_in_program_by_node(source, match.captures.filter(c => c.name === "closing-name")[0].node, "ChargeCard")
+            //     program.replace_in_program_by_node(match.captures.filter(c => c.name === "closing-name")[0].node, "ChargeCard")
 
             //     // broken: replace children of the element
-            //     source = replace_in_program_by_node(
-            //         source,
+            //     program.replace_in_program_by_node(
             //         match.captures.filter(c => c.name === "children")[0].node,
             //         change.upgrade
             //     )
@@ -117,14 +119,14 @@ const go = (change = null) => {
             //     match.captures.filter(c => c.name === "attr").reverse().forEach(c => {
             //         var attr_name = parsed.getText(c.node.children[0])
             //         if(attr_name === "code" || attr_name === "source")
-            //             source = replace_in_program_by_node(source, c.node, "")
+            //             program.replace_in_program_by_node(source, c.node, "")
             //     })
 
-            //     source = replace_in_program_by_node(source, match.captures.filter(c => c.name === "opening-name")[0].node, "ChargeCard")
+            //     program.replace_in_program_by_node(match.captures.filter(c => c.name === "opening-name")[0].node, "ChargeCard")
             // })
         }
 
-        var remade = source
+        var remade = program.source
 
         fs.writeFile(sourceAddress, remade, err => console.log(err))
     })
